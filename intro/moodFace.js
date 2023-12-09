5fa3897c2403a1074a20fdcced9dc5449a814480
Xiaotong Lin <xiaotong@Jessicas-MBP-81.hsd1.mi.comcast.net>
2023-12-09 13:22:32 -0500

    used subscribe for moodFace

--- intro/moodFace.js
+++ intro/moodFace.js
@@ -5,29 +5,26 @@ window.onload = function(e){
     const btnAngry = document.querySelector("#angry");
     btnAngry.addEventListener("click", function(e){
         store.dispatch({type: "ANGRY"});
-        const face = store.getState();
-        h1.innerText = EMOJIS[face.type];
     });
     
     const btnHappy = document.querySelector("#happy");
     btnHappy.addEventListener("click", function(e){
         store.dispatch({type: "HAPPY"});
-        const face = store.getState();
-        h1.innerText = EMOJIS[face.type];
     });
     
     const btnSad = document.querySelector("#sad");
     btnSad.addEventListener("click", function(e){
         store.dispatch({type: "SAD"});
-        const face = store.getState();
-        h1.innerText = EMOJIS[face.type];
     });
     
     const btnSurprise = document.querySelector("#surprise");
     btnSurprise.addEventListener("click", function(e){
         store.dispatch({type: "SURPRISE"});
+    });
+
+    store.subscribe(function(){
         const face = store.getState();
         h1.innerText = EMOJIS[face.type];
-    });
+    })
     
 };
